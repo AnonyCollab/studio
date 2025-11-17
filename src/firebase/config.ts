e78664db16b8c95e0a3592835e9b021fb1c76334
Anony Collab <anonycollab@gmail.com>
2025-11-17 00:50:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7236062529-ae5b7",
+  "appId": "1:482155352610:web:6c1481768b92a08e4bb07b",
+  "apiKey": "AIzaSyDdRweQHgmeYDI-Y2IUUm7alKlTsMrU50w",
+  "authDomain": "studio-7236062529-ae5b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "482155352610"
+};
